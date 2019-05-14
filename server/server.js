e309278948bd9b51d07f3186e68e2701eec024c8
Yanndeo <yan2sambou@gmail.com>
2019-05-14 10:47:57 +0000

    Install dependencies and express setup --back

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+//const connectDB = require('./config/db');
+
+const app = express();
+
+////Connect Database
+//connectDB();
+
+//Bodyparser Middleware
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('API Running')
+});
+
+
+
+
+// app.use('/api/users', require('./routes/api/users'));
+// app.use('/api/auth', require('./routes/api/auth'));
+// app.use('/api/profile', require('./routes/api/profile'));
+// app.use('/api/posts', require('./routes/api/posts'));
+
+
+
+const PORT = process.env.PORT || 8888;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
